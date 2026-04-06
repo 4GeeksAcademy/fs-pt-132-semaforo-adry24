793123fde539ccfc59b8110cea3451efd5e5c341
adry24 <160780142+adry24@users.noreply.github.com>
2026-04-06 18:37:03 +0000

    finalizado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 //Bootstrap
@@ -11,8 +11,44 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+function TrafficLight() {
+  const [color, setColor] = useState("red");
+  const [hasPurple, setHasPurple] = useState(false);
+
+  const colors = hasPurple
+    ? ["red", "yellow", "green", "purple"]
+    : ["red", "yellow", "green"];
+
+  const nextColor = () => {
+    const currentIndex = colors.indexOf(color);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    setColor(colors[nextIndex]);
+  };
+
+  return (
+    <div className="container">
+      <div className="traffic-light">
+        {colors.map((lightColor) => (
+          <div
+            key={lightColor}
+            onClick={() => setColor(lightColor)}
+            className={`light ${lightColor} ${
+              color === lightColor ? "selected" : ""
+            }`}
+          ></div>
+        ))}
+      </div>
+
+      <button onClick={nextColor}>Cambiar color</button>
+
+      {!hasPurple && (
+        <button onClick={() => setHasPurple(true)}>
+          Añadir púrpura
+        </button>
+      )}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<TrafficLight />);
